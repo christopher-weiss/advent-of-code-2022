13c539f5dd28b464c7b7ebb378c804840ba9c424
Christopher Weiss <mail@chris-weiss.net>
2022-12-03 20:49:39 +0100

    Implement Day 3 - Part 2

--- day3/day3.go
+++ day3/day3.go
@@ -9,36 +9,75 @@ import (
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("/Users/cu/Repositories/advent-of-code-2022/day3/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var sum int
+	var sumPart1, sumPart2 int
+
+	groups := make([][]string, 500)
+	groups[0] = make([]string, 3)
+
+	var groupIndex, groupSubIndex int
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		rucksack := scanner.Text()
-		comp1, comp2 := seperateIntoCompartments(rucksack)
 
+		// Part 1
+		comp1, comp2 := seperateIntoCompartments(rucksack)
 		for _, r := range comp1[:] {
 			if strings.ContainsRune(comp2, r) {
-				sum += priority(r)
+				sumPart1 += priority(r)
 				break
 			}
 		}
+
+		// Part 2
+		if groupSubIndex%3 == 0 {
+			groupIndex++
+			groups[groupIndex] = make([]string, 3)
+			groupSubIndex = 0
+		}
+		groups[groupIndex][groupSubIndex] = rucksack
+		groupSubIndex++
 	}
-	fmt.Printf("Sum: %d\n", sum)
+
+	for _, group := range groups {
+		if group != nil {
+			sumPart2 += priority(findBadge(group))
+		}
+	}
+
+	fmt.Printf("Sum part 1: %d\n", sumPart1)
+	fmt.Printf("Sum part 2: %d\n", sumPart2)
+
 }
 
 func seperateIntoCompartments(rucksack string) (string, string) {
-	size := int(len(rucksack) / 2)
+	size := len(rucksack) / 2
 
-	return rucksack[0:size], rucksack[size:]
+	return rucksack[:size], rucksack[size:]
 }
 
 func priority(item rune) int {
 	priorities := ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	return strings.Index(priorities, string(item))
+}
+
+func findBadge(rucksacks []string) rune {
+	candidates := make([]rune, 0)
+	for _, r1 := range rucksacks[0] {
+		if strings.ContainsRune(rucksacks[1], r1) {
+			candidates = append(candidates, r1)
+		}
+	}
+	for _, r2 := range candidates {
+		if strings.ContainsRune(string(rucksacks[2]), r2) {
+			return r2
+		}
+	}
+	return '.'
 }
