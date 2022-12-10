69d0cb098d3c819b48b33307f6b35bac1282a04e
Christopher Weiss <mail@chris-weiss.net>
2022-12-10 22:47:33 +0100

    Implement Day 10 - Part 2

--- day10/day10.go
+++ day10/day10.go
@@ -8,7 +8,10 @@ import (
 	"strconv"
 	"strings"
 )
-
+const (
+	crtWidth int = 40
+	crtHeight int = 6
+)
 var x, cycle int
 var signalMap map[int]int
 
@@ -42,4 +45,23 @@ func main() {
 
 	solution1 := signalMap[20] * 20 + signalMap[60] * 60 + signalMap[100] * 100 + signalMap[140] * 140 + signalMap[180] * 180 + signalMap[220] * 220
 	fmt.Println(solution1)
+
+	drawCrt()
+}
+
+func drawCrt() {
+	for i := 1; i <= crtHeight * crtWidth; i++ {
+		char := ""
+		pos := (i%crtWidth)-1
+		if signalMap[i] == pos || signalMap[i] == pos-1 || signalMap[i] == pos+1 {
+			char = "#"
+		} else {
+			char = "."
+		}
+		if i % crtWidth == 0 {
+			fmt.Printf("%s\n", char)
+		} else {
+			fmt.Printf("%s", char)
+		}
+	}
 }
